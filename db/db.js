0e76a57e9acf4fce065b3d292f98697a4af5b73c
urosrelic <urosrelicc@gmail.com>
2024-05-13 18:41:56 +0200

    feat: add password field to user table

--- db/db.js
+++ db/db.js
@@ -35,6 +35,7 @@ connection.connect((err) => {
 
       last_name varchar(255) DEFAULT NULL,
       username varchar(255) DEFAULT NULL,
+        password varchar(255) DEFAULT NULL,
       role varchar(255) DEFAULT NULL,
       PRIMARY KEY (id)
     ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
